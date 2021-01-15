d282e924bf7908a9ab66782e13c38d6a77ec411e
unknown <bruno.pariss@hotmail.comgit config --global user.email bruno.pariss@hotmail.com>
2021-01-15 16:01:18 -0300

    Comitando projeto DDD Avanade

---
+++ Aurora.Domain/Interfaces/IRepositoryWorker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Aurora.Domain.Entities;
+
+namespace Aurora.Domain.Interfaces
+{
+    public interface IRepositoryWorker
+    {
+        void Save(Worker obj);
+
+        void Remove(int id);
+
+        Worker GetById(int id);
+
+        IList<Worker> GetAll();
+
+    }
+}
+
